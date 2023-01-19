e3b7c918793e60bac8af2844ba671677ccf42509
Clement Alasan <mejortech>
2023-01-19 16:46:54 +0100

    first commit

---
+++ assets/javaScript/main.js
@@ -0,0 +1,12 @@
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.querySelector('#header')
+
+    if(this.scrollY >= 50 ){ 
+        header.classList.add('scroll-header')
+    }else{
+        header.classList.remove('scroll-header')
+    }
+}
+window.addEventListener('scroll', scrollHeader)
